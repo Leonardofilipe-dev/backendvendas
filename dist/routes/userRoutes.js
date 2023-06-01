e784ee6f1d65b2c01283743070de2de459c489ab
Leonardo-dev <leonardo.filipe@yahoo.com>
2023-05-31 22:12:57 -0300

    Adicionando login e tpken

--- dist/routes/userRoutes.js
+++ dist/routes/userRoutes.js
@@ -5,9 +5,13 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = require("express");
 const userController_1 = __importDefault(require("../controller/userController"));
+const authController_1 = __importDefault(require("../controller/authController"));
+const userValidate_1 = __importDefault(require("../validate/userValidate"));
+const login_1 = __importDefault(require("../validate/auth/login"));
 const routes = (0, express_1.Router)();
 routes
-    .post("/user", userController_1.default.create)
+    .post("/user", userValidate_1.default, userController_1.default.create)
+    .post("/login", login_1.default, authController_1.default.login)
     .get("/user", userController_1.default.getAll)
     .get("/user/:id", userController_1.default.getById)
     .put("/user/:id", userController_1.default.update)
--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,11 +1,15 @@
 import { Router } from "express";
 import Controller from "../controller/userController";
+import authController from "../controller/authController";
+import userValidate from "../validate/userValidate";
+import authLogin from "../validate/auth/login"
 
 
 const routes = Router()
 
 routes
-.post("/user", Controller.create)
+.post("/user", userValidate, Controller.create)
+.post("/login", authLogin, authController.login)
 .get("/user", Controller.getAll)
 .get("/user/:id", Controller.getById)
 .put("/user/:id", Controller.update)
